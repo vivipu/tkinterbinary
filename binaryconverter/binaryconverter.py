dd3e90506cc7a41c2d2fc986955e954358b38de7
vivi <41368076+veevyo@users.noreply.github.com>
2022-12-15 15:49:44 -0600

    bugfix and refractoring

--- binaryconverter/binaryconverter.py
+++ binaryconverter/binaryconverter.py
@@ -1,42 +1,66 @@
 from tkinter import * 
-#insert output function
-def click():
+
+def intcheck(text):
     numbers = "0123456789"
-    entered_text = textentry.get()
-    #amount of numbers in text
-    nums = 0
-    #amount of binary values in text
-    bincount = 0
+    nums = 0;
+    for x in text:
+        if x in numbers or x.isspace():
+            nums += 1
+    return nums;
+
+def bincheck(text):
+    bincount = 0;
+    for x in text:
+        if x == "0" or x == "1" or x.isspace():
+            bincount += 1
+    return bincount;
+
+def stringtobin(text):
+    spaceless = text.replace(" ", "")
+    out = "".join(format(ord(x), "08b") for x in spaceless)
+    return out
+
+def inttobin(text):
+    spaceless = text.replace(" ", "")
+    out = bin(int(spaceless)).replace("0b","")
+    return out
+
+def bintostr(text):
+    spaceless = text.replace(" ", "")
+    #try making the output be a converted string first
+    out = ''.join(chr(int(spaceless[i*8:i*8+8],2)) for i in range(len(spaceless)//8))
+    #make the output convert back into binary for testing purposes
+    test = "".join(format(ord(x), "08b") for x in out).replace("0b","")
+    #if the binary test does not equal the initial entered text, the output is an integer instead
+    if not out or spaceless != test:
+        arr = list(spaceless)
+        out = int("".join([str(x) for x in arr]), 2)
+    return out;
+
+def convert(text):
     out = ""
     #clear output
     output.delete(0.0, END)
-    #check if input is int, check if binary was entered
-    for x in entered_text:
-        if x in numbers:
-            nums += 1
-        if x == "0" or x == "1":
-            bincount += 1
-    #if input is not an int (therefore it is a string), output = the binary for the given string
-    if nums != len(entered_text):
-        out = "".join(format(ord(x), "08b") for x in entered_text)
-    #if input is an int, output = the binary for the given int
-    elif nums == len(entered_text) and nums != bincount:
-        out = bin(int(entered_text)).replace("0b","")
+    nums = intcheck(text)
+    bins = bincheck(text)
+    if nums != len(text):
+        out = stringtobin(text);
+    elif nums == len(text) and nums != bins:
+        out = inttobin(text);
     #if input is binary, convert
-    elif len(entered_text) == bincount:
-        #try making the output be a converted string first
-        out = ''.join(chr(int(entered_text[i*8:i*8+8],2)) for i in range(len(entered_text)//8))
-        #make the output convert back into binary for testing purposes
-        test = "".join(format(ord(x), "08b") for x in out).replace("0b","")
-        #if the binary test does not equal the initial entered text, the output is an integer instead
-        if not out or entered_text != test:
-            arr = list(entered_text)
-            out = int("".join([str(x) for x in arr]), 2)
+    elif len(text) == bins:
+        out = bintostr(text);
+    return out
+
+def click():
+    entered_text = textentry.get()
     #insert output
-    output.insert(END, out)
+    output.insert(END, convert(entered_text))
+
 #window close function
 def close_window():
     window.destroy()
+
 #window config
 window = Tk()
 window.title("Binary converter")
